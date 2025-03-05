1a49c3af56274fef1f2ac86479d6d8e7cfadeeef
pratikj17 <jpratik4002@gmail.com>
2025-03-05 18:19:43 +0530

    db change(local to atlas)

--- backend/connection.js
+++ backend/connection.js
@@ -1,7 +1,8 @@
 const mongoose=require('mongoose');
+require('dotenv').config()
 
 function connectToDB(url){
-    mongoose.connect(url).then(()=>console.log("DB connected!"));
+    mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.3eot9.mongodb.net/Users?retryWrites=true&w=majority&appName=Cluster0`).then(()=>console.log("DB connected!"));
 }
 
 module.exports={connectToDB};
